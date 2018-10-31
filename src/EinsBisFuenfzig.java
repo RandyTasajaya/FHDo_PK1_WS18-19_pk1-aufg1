b9505bc4f1deb9501bbe5de9f909f843f50bcdf6
randy <randy@172.22.204.33>
2018-10-31 13:28:09 +0100

    Erste Änderung (Aufg. 2.3 - 2.4)

--- src/EinsBisFuenfzig.java
+++ src/EinsBisFuenfzig.java
@@ -2,6 +2,13 @@ public class EinsBisFuenfzig
 {
 	public static void main(String[] args) {
 		
+		if(args.length == 1) {
+			int zahl = Integer.parseInt(args[0]);
+			for(int i = 1; i <= zahl; i++) {
+				System.out.print(i + " ");
+			}
+		}
+		else
 		for(int i = 1; i <= 50; i++) {
 			if(i % 3 == 0 && i % 5 == 0) {
 				System.out.print("PingPong" + " ");
