70fa2eb97f05902b5a0c7e9892f5606f9e714a61
Randy Tasajaya <randy.tasajaya001@stud.fh-dortmund.de>
2018-12-22 01:38:23 +0100

    Finished configuring IntelliJ Ultimate Edition

--- src/EinsBisFuenfzig.java
+++ src/EinsBisFuenfzig.java
@@ -1,31 +1,31 @@
 public class EinsBisFuenfzig
 {
-	public static void main(String[] args) {
-		
-		if(args.length == 1) {
-			int zahl = Integer.parseInt(args[0]);
-			for(int i = 1; i <= zahl; i++) {
-				System.out.print(i + " ");
-			}
-		}
-		else
-		for(int i = 1; i <= 50; i++) {
-			if(i % 3 == 0 && i % 5 == 0) {
-				System.out.print("PingPong" + " ");
-			} else {
-				if(i % 3 == 0) {
-					System.out.print("Ping" + " ");
-				} else {
-					if(i % 5 == 0) {
-						System.out.print("Pong" + " ");
-					} else {
-						System.out.print(i + " ");
-					}
-				}
-			}
-		}
+    public static void main(String[] args) {
 
-	}
+        if(args.length == 1) {
+            int zahl = Integer.parseInt(args[0]);
+            for(int i = 1; i <= zahl; i++) {
+                System.out.print(i + " ");
+            }
+        }
+        else
+            for(int i = 1; i <= 50; i++) {
+                if(i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("PingPong" + " ");
+                } else {
+                    if(i % 3 == 0) {
+                        System.out.print("Ping" + " ");
+                    } else {
+                        if(i % 5 == 0) {
+                            System.out.print("Pong" + " ");
+                        } else {
+                            System.out.print(i + " ");
+                        }
+                    }
+                }
+            }
+
+    }
 }
 
 // Eine Aenderung im Editor von Gitlab
